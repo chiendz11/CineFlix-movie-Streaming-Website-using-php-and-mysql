a1379f0477638691f3f1cf5d7fa64e0f85f5caa1
chiendz11 <thuha25121976@gmail.com>
2024-11-20 16:40:08 +0700

    a bit changes

--- main/conn.php
+++ main/conn.php
@@ -1,4 +1,8 @@
 <?php
-$conn=mysqli_connect("localhost","root","","insert");
+$conn = mysqli_connect("localhost", "root", "", "movieweb");
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 ?>
